f44202c43eab5b4ffe38577a72c69c2515dccf50
Ricardo Alencar <ricfilho0007@gmail.com>
2023-08-23 22:25:21 -0300

    create func

---
+++ handlers/create.go
@@ -0,0 +1,39 @@
+package handlers
+
+import (
+	"api-postgresql/models"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	var todo models.Todo
+
+	err := json.NewDecoder(r.Body).Decode(*&todo)
+	if err != nil {
+		log.Printf("Erro ao fazer decode do json: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := models.Insert(todo)
+
+	var resp map[string]any
+
+	if err != nil {
+		resp = map[string]any{
+			"Error":   true,
+			"Message": fmt.Sprintf("Ocorreu um erro ao tentar inserir: %v", err),
+		}
+	} else {
+		resp = map[string]any{
+			"Error":   false,
+			"Message": fmt.Sprintf("ToDo inserido com sucesso! ID:: %d", id),
+		}
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(resp)
+}
